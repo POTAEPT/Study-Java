41fcf543f47d9f664f225b2e4e9b5f6d7946c43d
Potae <nattwutramee@gmail.com>
2025-10-25 14:02:52 +0700

    Refactor login system and improve user input validation

--- Study/src/Study_forExam.java
+++ Study/src/Study_forExam.java
@@ -2,30 +2,53 @@ import java.util.Scanner;
 
 public class Study_forExam {
 
-    String[][] user = new String[5][2];
+    static  String[][] user = new String[5][2];
+    static  int userCount = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-        System.out.print("""
+        while (true) { 
+            System.out.print("""
                 ============ Welcome To Program ============
                 Press L to Login or Press E to Exit Program
                 """);
-        System.out.print(":");
-        String input = sc.nextLine().trim().toUpperCase();
-        switch (input){
-            case "L": loginSytem(sc); break;
-            case "E": return;
-            default: System.err.print("Error Invalid choice : Press L or E only");
+            System.out.print(":");
+            String input = sc.nextLine().trim().toUpperCase();
+            switch (input){
+                case "L": loginSystem(sc); break;
+                case "E": System.out.println("Exit Program"); return;
+                default: System.err.println("Error Invalid choice : Press L or E only");
+            }
         }
+        
     }
 
-    public static void loginSytem (Scanner sc){
+    public static void loginSystem (Scanner sc){
         System.out.println("===== Login =====");
         System.out.print("Username: ");
         String username = sc.nextLine().trim();
         System.out.print("Password: ");
         String password = sc.nextLine().trim();
+        
+        if(username.isEmpty() || password.isEmpty()){
+            System.err.println("Invalid input : Input Username and Password");
+        }
+        
+        boolean checking = checkLogin(user, userCount, username, password);
+        if(checking){
+            System.out.println("===== Hello "+ username + " =====");
+        }else{System.out.println("User not found : username or password is wrong");}
+    }
 
+    public static boolean checkLogin(String[][] users, int userCount, String username , String password){
+        if(username == null || password == null){
+            return false;
+        }
+        for (int i = 0; i < userCount; i++){
+            if(user[i] != null && users[i][0].equals(username) && users[i][1].equals(password)){
+                return true;
+            }
+        }
+        return false;
     }
 }
